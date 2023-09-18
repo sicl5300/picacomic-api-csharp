022182e615d1d0673976fac3242a803d7cb1f69a
Joseph D.X <47028479+sicl5300@users.noreply.github.com>
2023-09-18 14:07:36 +0800

    [Abstractions] 创建`WrappedResponse(T)`包装标准API返回值
    用于JSON反序列化

---
+++ src/PicacomicSharp/Responses/Abstractions/WrappedResponse.cs
@@ -0,0 +1,33 @@
+using System.Text.Json.Serialization;
+
+namespace PicacomicSharp.Responses.Abstractions;
+
+/// <summary>
+///     标准API返回内容的包装，包含<see cref="Code" />、<see cref="Error" />、<see cref="Message" />和<see cref="Data" />。
+/// </summary>
+/// <typeparam name="T"><see cref="IResponseData" />，即包装在内的<see cref="Data"/></typeparam>
+public class WrappedResponse<T> where T : IResponseData
+{
+    [JsonPropertyName("code")]
+    [JsonNumberHandling(JsonNumberHandling.AllowReadingFromString)]
+    public int Code { get; set; }
+
+    /// <summary>
+    ///     有错误时才不为<c>null</c>，表示错误代码。
+    /// </summary>
+    [JsonPropertyName("error")]
+    [JsonNumberHandling(JsonNumberHandling.AllowReadingFromString)]
+    public int? Error { get; set; } = default;
+
+    [JsonPropertyName("message")]
+    public string? Message { get; set; } = default;
+
+    /// <summary>
+    ///     实际数据。
+    /// </summary>
+    [JsonPropertyName("data")]
+    public T? Data { get; set; } = default;
+
+    public bool IsSuccess => Code == 200;
+    public bool IsError => Error is not null;
+}
