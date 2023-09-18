1d3fe8a61e6586b3cf2509b7d0495b73d9b1e4ea
Joseph D.X <47028479+sicl5300@users.noreply.github.com>
2023-09-18 14:41:44 +0800

    [PicaClient] 添加统一的请求发送方法
    输入请求设置，自动设置header，填入信息，发送请求，获得标准api内容返回结构，判断请求是否成功。

--- src/PicacomicSharp/PicaClient.cs
+++ src/PicacomicSharp/PicaClient.cs
@@ -1,4 +1,8 @@
+using System.Text;
+using System.Text.Json;
 using PicacomicSharp.DependencyInjection;
+using PicacomicSharp.Requests.Abstractions;
+using PicacomicSharp.Responses.Abstractions;
 
 namespace PicacomicSharp;
 
@@ -19,5 +23,38 @@ public class PicaClient
         }
 
         Client = client;
+    }
+
+    internal async Task<TResponse> SendAsync<TRequest, TResponse>(TRequest config)
+        where TResponse : IResponseData
+        where TRequest : IRequestData
+    {
+        var method = config switch
+        {
+            IPost => HttpMethod.Post,
+            IGet => HttpMethod.Get,
+            _ => throw new ArgumentOutOfRangeException(nameof(config), config, null)
+        };
+        
+        var requestMessage = new HttpRequestMessage(method, config.Url);
+        
+        if (config is IPost)
+        {
+            requestMessage.Content = new StringContent(JsonSerializer.Serialize(config), Encoding.UTF8, "application/json");
+        }
+        
+        var response = await Client.SendAsync(requestMessage);
+        var message = await response.Content.ReadAsStringAsync();
+
+        var wrapped = JsonSerializer.Deserialize<WrappedResponse<TResponse>>(message, new JsonSerializerOptions
+        {
+            PropertyNameCaseInsensitive = true
+        })!;
+        if (wrapped.Data is not null) return wrapped.Data;
+
+        if (wrapped.Error is not null) // Data is null, but Error is not null
+            throw new InvalidOperationException($"Error: {wrapped.Error}, Message: {wrapped.Message ?? "None"}.");
+
+        throw new InvalidOperationException("Invalid response.");
     }
 }
