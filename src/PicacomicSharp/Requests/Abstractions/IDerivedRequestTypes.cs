77c798a8d16a621e7fe18db0030316735b690647
Joseph D.X <47028479+sicl5300@users.noreply.github.com>
2023-09-18 14:43:22 +0800

    Bugfix [Abstractions] 删除`IPost`中的请求数据属性
    删除`DataSerialized`，因为PicaClient中的统一发送方法`SendAsync`泛型约束无法限定为`IPost`，想要访问到必须强制转换过去，但是这样`JsonSerializer.Serialize(this)`就无法工作了。
    
    感觉`System.Text.Json`这玩意真不如`Newtonsoft.Json`无脑、简单、方便，可惜是标准库。

--- src/PicacomicSharp/Requests/Abstractions/IDerivedRequestTypes.cs
+++ src/PicacomicSharp/Requests/Abstractions/IDerivedRequestTypes.cs
@@ -6,11 +6,6 @@ namespace PicacomicSharp.Requests.Abstractions;
 
 public interface IPost : IRequestData
 {
-    /// <summary>
-    ///     序列化好的POST请求数据。
-    /// </summary>
-    [JsonIgnore] public StringContent DataSerialized =>
-        new StringContent(JsonSerializer.Serialize(this), Encoding.UTF8, "application/json");
 }
 
 public interface IGet : IRequestData
